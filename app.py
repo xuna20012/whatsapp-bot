58bbb07e5eafc345eb150f05019239421e570435
xuna20012 <101480707+xuna20012@users.noreply.github.com>
2022-09-08 20:43:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,91 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
-from pymongo import MongoClient
-from datetime import datetime
+from wa_automate_socket_client import SocketClient
+from wati_api import client
+from time import sleep
 
-cluster = MongoClient("mongodb+srv://xuna20012:7815@cluster0.r0hwd.mongodb.net/?retryWrites=true&w=majority")
-db = cluster["xunabot"]
-users = db["users"]
-orders = db["orders"]
+client = SocketClient('http://localhost:5000/')
 
-app = Flask(__name__)
-@app.route("/", methods=["get", "post"])
+def reply(message):
+    data = message["data"]
+    text = data["text"]
+    number = data["from"]
+    id = data["id"]
 
-def reply():
-    text = request.form.get("Body")
-    number = request.form.get("From")
-    number = number.replace("whatsapp:", "")
-    res = MessagingResponse()
-    user = users.find_one({"number": number})
-
-    if bool(user) == False:
-        res.message("Hi, thank for contacting *Xuna Cakes*.\nYou can choose from one of the options below: "
-                    "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
-                    "To get our *address*")
-        users.insert_one({"number": number, "status": "main", "messages": []})
-    elif user["status"] == "main":
-        try:
-            option = int(text)
-        except:
-            res.message("Svp veuiller entrer une reponse valide")
-            return str(res)
-
-        if option == 1:
-            res.message("Nunemro Telephone: 781745898 \nE-mail* : cheikhounafall20012@gmail.com")
-
-        elif option == 2:
-            res.message("You have entered *ordering mode*.")
-            users.update_one({"number": number}, {"$set": {"status": "ordering"}})
-            res.message("You can select one of the following cakes to order: \n\n1️⃣ Red Velvet  \n2️⃣ Dark Forest \n3️⃣ Ice Cream Cake"
-                "\n4️⃣ Plum Cake \n5️⃣ Sponge Cake \n6️⃣ Genoise Cake \n7️⃣ Angel Cake \n8️⃣ Carrot Cake \n9️⃣ Fruit Cake  \n0️⃣ Go Back")
-
-        elif option == 3:
-            res.message("Nous travaillons tous les jours de *8h a 00h* ")
-
-        elif option == 4:
-            res.message("Nous sommes au Scat Urbam a cote du Prestige Deco Villa 45")
-
-        else:
-            res.message("Svp veuiller entrer une reponse valide")
-            return str(res)
-
-    elif user["status"] == "ordering":
-        try:
-            option = int(text)
-        except:
-            res.message("Svp veuiller entrer une reponse valide")
-            return str(res)
-
-        if option == 0:
-            users.update_one({"number": number}, {"$set": {"status": "main"}})
-            res.message("You can choose from one of the options below: "
-                        "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
-                        "To get our *address*")
-
-        elif 1 <= text <= 9:
-            cakes = ["Red Velvet", "Dark Forest", "Ice Cream Cake", "Plum Cake", "Sponge Cake", "Genoise Cake", "Angel Cake", "Carrot Cake"]
-            selected = cakes[option -1]
-            users.update_one({"number": number}, {"$set": {"status": "address"}})
-            users.update_one({"number": number}, {"$set": {"item": selected}})
-            res.message("Excellente choice 😉")
-            res.message("Please enter your address to confirm this order")
-        else:
-            res.message("Svp veuiller entrer une reponse valide")
-
-    elif user["status"] == "address":
-        selected = user["item"]
-        res.message("Thanks for shopping with us !")
-        res.message(f"Your order for {selected} hase been recieved and will be delivered within an houre")
-        orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
-        users.update_one({"number": number}, {"$set": {"status": "ordered"}})
-    elif user["status"] == "ordered":
-        res.message("Hi, thank for contacting again.\nYou can choose from one of the options below: "
-                    "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
-                    "To get our *address*")
-        users.update_one({"number": number}, {"$set": {"status": "main"}})
+    if "Hello" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
 
 
-    users.update_one({"number": number}, {"$push": {"messages": {"text": text, "date": datetime.now()}}})
-    return str(res)
+    if "Salut" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
 
-if __name__ == "__main__":
-    app.run()
+    if "Bonsoir" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+    if "bonsoir" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+    if "Hi" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+
+    if "Bonjour" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+    if "bonjour" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+    if "Bnjr" in text:
+        client.sendText(number, "Bienvenu sur le REBOT de *CheiXuna*.\nveuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+        client.sendImage(number, "")
+    if "Merci" in text:
+        client.sendText(number, "Merciiii A bientot")
+    if "Ok" in text:
+        client.sendText(number, "Merciiii A bientot")
+    if "ok" in text:
+        client.sendText(number, "Merciiii A bientot")
+    if "merci" in text:
+        client.sendText(number, "Merciiii A bientot")
+    if "Excellent" in text:
+        client.sendText(number, "Merciiii A bientot")
+    if text == "1":
+        client.sendText(number,
+                        "Merci de suivre votre commande: \n\n *5:* Commander une *Pizza*\n *6:* Commander un *Burger*\n "
+                        "*7:* Commander un *Tacos*\n *0:* Retourner au Menu Principal")
+    if "0" in text:
+        client.sendText(number, "veuiller choisir parmis les options suivantes: "
+                                "\n\n*Tapez*\n\n *1:* Pour *commander* \n *2:* Pour Connaitre nos *heures de travails* \n *3:* Pour nous *contacter* "
+                                "\n *4:* Pour connaitre notre *adresse*")
+
+    elif text == "2":
+        client.sendText(number,
+                        "Lundi : De 8h:00 a 00H00\nMardi : De 8h:00 a 00H00\nMercredi : De 8h:00 a 00H00\nJeudi : De 8h:00 a 00H00\n"
+                        "Vendredi : De 8h:00 a 00H00\nSamedi : De 9h:00 a 00H00\nDimanche : De 9h:00 a 23H00\n\n\n"
+                        "*0:* Retourner au Menu Principal")
+    elif text == "3":
+        client.sendText(number,
+                        "*Nos contacts:*\nTelephone : 781745898\nMail : cheikhounafall20012@gmail.com\nInstagram : @xuna_fall\n\n\n"
+                        "*0:* Retourner au Menu Principal")
+    elif text == "4":
+        client.sendText(number, "Nous sommes aux HLM Grand Yoff\n\n\n*0:* Retourner au Menu Principal")
+    elif text == "5":
+        client.sendText(number, "*Woplow !! Nos Pizza sont les meilleurs !!*\n\n")
+        client.sendImage(number, "images/Pizza.jpg", "image", "*Menu*\n\n *0:* Retourner au Menu Principal")
+    elif text == "6":
+        client.sendText(number, "*Woplow !! Nos Burger sont les meilleurs !!*\n\n")
+        client.sendImage(number, "images/Burger.jpeg", "image", "*Menu*\n\n *0:* Retourner au Menu Principal")
+    elif text == "7":
+        client.sendText(number, "*Woplow !! Nos Tacos sont les meilleurs !!*\n\n")
+        client.sendImage(number, "img/biff.jpg", "image", "*Menu*\n\n *0:* Retourner au Menu Principal")
+
+
+client.onMessage(reply)
